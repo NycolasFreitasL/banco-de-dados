5f201c341747c07a320ed48d434252f2f0a79126
Nycolas Freitas Santos Leal <159497049+NycolasFreitasL@users.noreply.github.com>
2025-05-21 12:45:21 -0300

    Add files via upload

--- sql/etecmcm2.sql
+++ sql/etecmcm2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14-Maio-2025 às 17:41
+-- Tempo de geração: 21-Maio-2025 às 17:44
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -60,30 +60,31 @@ CREATE TABLE `produtos` (
   `preco` decimal(10,2) DEFAULT NULL,
   `quant` int(11) DEFAULT NULL,
   `marca` varchar(255) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
+  `id_categoria` int(11) DEFAULT NULL,
+  `unidade_medida` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`) VALUES
-(1, 'sabão em pó', '30.50', 50, 'omo', 1),
-(2, 'detergente', '3.55', 10, 'ypê', 1),
-(3, 'shampoo', '30.00', 50, 'dove', 2),
-(4, 'sabonete', '3.00', 50, 'dove', 2),
-(5, 'peito de peru', '8.00', 70, 'seara', 3),
-(6, 'mussarela', '4.00', 80, 'sadia', 3),
-(7, 'sardinha', '2.00', 40, 'coqueiro', 4),
-(8, 'milho', '3.00', 48, 'cepera', 4),
-(9, 'melancia', '3.00', 28, 'mineiro', 6),
-(10, 'tamara', '7.00', 38, 'mineiro', 6),
-(11, 'requeijão', '7.60', 20, 'holandes', 7),
-(12, 'manteiga', '7.90', 15, 'presidente', 7),
-(13, 'televisão', '700.90', 15, 'philco', 8),
-(14, 'geladeira', '799.90', 10, 'samsung', 8),
-(15, 'camiseta', '70.80', 18, 'nike', 9),
-(16, 'calça', '59.90', 28, 'adidas', 9);
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`, `unidade_medida`) VALUES
+(1, 'sabão em pó', '30.50', 50, 'omo', 1, 'un'),
+(2, 'detergente', '3.55', 10, 'ypê', 1, 'un'),
+(3, 'shampoo', '30.00', 50, 'dove', 2, NULL),
+(4, 'sabonete', '3.00', 50, 'dove', 2, NULL),
+(5, 'peito de peru', '8.00', 70, 'seara', 3, 'g'),
+(6, 'mussarela', '4.00', 80, 'sadia', 3, 'g'),
+(7, 'sardinha', '2.00', 40, 'coqueiro', 4, 'un'),
+(8, 'milho', '3.00', 48, 'cepera', 4, 'un'),
+(9, 'melancia', '3.00', 28, 'mineiro', 6, 'g'),
+(10, 'tamara', '7.00', 38, 'mineiro', 6, 'g'),
+(11, 'requeijão', '7.60', 20, 'holandes', 7, 'un'),
+(12, 'manteiga', '7.90', 15, 'presidente', 7, 'un'),
+(13, 'televisão', '700.90', 15, 'philco', 8, 'un'),
+(14, 'geladeira', '799.90', 10, 'samsung', 8, 'un'),
+(15, 'camiseta', '70.80', 18, 'nike', 9, 'un'),
+(16, 'calça', '59.90', 28, 'adidas', 9, 'un');
 
 --
 -- Índices para tabelas despejadas
